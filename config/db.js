7f6c433b79e62e77e91ab537ea384669ed2bd0fa
Satang090144 <developer.satang.pdm@gmail.com>
2026-06-12 14:52:42 +0700

    db-config

--- config/db.js
+++ config/db.js
@@ -7,13 +7,19 @@ const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,      
     dialect: "mysql",
     logging: false,
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false   
+      }
+    }
   }
-); 
+);
 
 sequelize.authenticate()
   .then(() => console.log('Database: connected'))
-  .catch(err => console.error(' DB connection error:', err));
+  .catch(err => console.error('DB connection error:', err));
 
 module.exports = sequelize;
